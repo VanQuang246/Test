acbae197125045cb5965e89010635f46bd986605
vanquangpro3 <34278583+vanquangpro3@users.noreply.github.com>
2018-09-28 15:32:40 +0700

    BaiTap

---
+++ cau4.php
@@ -0,0 +1,111 @@
+
+        <?php
+                        $users = array(
+                                0 => array(
+                                        'id' => 10, 
+                                        'name' => 'Messi',
+                                        'email' => array(
+                                                'gmail' => '10@gmail.com',
+                                                'yahoo' => '10@yahoo.com',
+                                        ),
+                                 ),                
+                                 1=> array(
+                                        'id' => 7,
+                                        'name' => 'Ronaldo',
+                                        'email' => array(
+                                                'gmail' => '7@gmail.com',
+                                                'yahoo' => '7@yahoo.com',
+                                        ),
+                                ),
+                                2=> array(
+                                        'id' => 4, 
+                                        'name' => 'Ramos',
+                                        'email' => array(
+                                                'gmail' => '4@gmail.com',
+                                                'yahoo' => '4@yahoo.com',
+                                        ),
+                                ),
+                                3=> array(
+                                        'id' => 13, 
+                                        'name' => 'Muller',
+                                        'email' => array(
+                                                'gmail' => '13@gmail.com',
+                                                'yahoo' => '13@yahoo.com',
+                                        ),
+                                ),
+                                4=> array(
+                                        'id' => 11, 
+                                        'name' => 'Neymar',
+                                        'email' => array(
+                                                'gmail' => '11@gmail.com',
+                                                'yahoo' => '11@yahoo.com',
+                                        ),
+                                ),
+                        );
+                ?>
+<!DOCTYPE html>
+       <html>
+       <head>
+               <title>Danh Sach User</title>
+       </head>
+       <body>
+        <center>
+                <table border="5">                 
+                        <tr>
+                                <td>STT</td>            
+                                <td>ID</td>     
+                                <td>Name</td>   
+                                <td>Email</td>
+                        </tr>   
+                                <?php for ($i = 0;$i < count($users); $i++) :?>
+                                        <tr>
+
+                                                <td><?php echo $i+1;?></td>
+                                                <td><?php echo $users[$i]['id'];?></td>
+                                                <td><?php echo $users[$i]['name'];?></td>
+                                                <td><?php echo $users[$i]['email']['gmail'];?></td>
+                                                <td><?php echo $users[$i]['email']['yahoo'];?></td>
+                                        </tr>               
+                        </tr> 
+                                <?php endfor; 
+                                ?>
+                </table>
+                <?php
+                for ($i=0; $i < count($users); $i++):
+                {
+                        if($users[$i]['id'] == 15) 
+                        {
+                                echo "Co id 15";
+                                return;
+                        }
+
+                }
+        endfor;
+        echo "Khong co ID 15";
+        ?>
+        </center>
+          <center>
+                <table border="5">                 
+                        <tr>
+                                <td>STT</td>            
+                                <td>ID</td>     
+                                <td>Name</td>   
+                                <td>Email</td>
+                        </tr>
+                        <?php sort($users)?>
+                                <?php for ($i = 0;$i < count($users); $i++) :?>
+                                        <tr>
+
+                                                <td><?php echo $i+1;?></td>
+                                                <td><?php echo $users[$i]['id'];?></td>
+                                                <td><?php echo $users[$i]['name'];?></td>
+                                                <td><?php echo $users[$i]['email']['gmail'];?></td>
+                                                <td><?php echo $users[$i]['email']['yahoo'];?></td>
+                                        </tr>               
+                        </tr> 
+                                <?php endfor; 
+                                ?>
+                </table>
+        </center>
+       </body>
+       </html>   
